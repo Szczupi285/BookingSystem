be30e08341aa5e11692784ca5b26346b4367ba38
unknown <bartekbiexd@gmail.com>
2024-08-02 00:02:53 +0200

    feat: HouseNumber value object

---
+++ src/BookingSystem.Domain/Exceptions/Location/HouseNumber/InvalidHouseNumberLengthException.cs
@@ -0,0 +1,16 @@
+﻿using BookingSystem.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingSystem.Domain.Exceptions.Location.HouseNumber
+{
+    public class InvalidHouseNumberLength : BookingDomainException
+    {
+        public InvalidHouseNumberLength(int maxLen) : base($"House number length cannot be more than {maxLen} characters")
+        {
+        }
+    }
+}
---
+++ src/BookingSystem.Domain/ValueObjects/Location/HouseNumber.cs
@@ -0,0 +1,24 @@
+﻿using BookingSystem.Domain.Exceptions.Location.HouseNumber;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingSystem.Domain.ValueObjects.Location
+{
+    public sealed record HouseNumber
+    {
+        public string Value { get; }
+
+        public HouseNumber(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                throw new HouseNumberCannotBeNullOrEmptyException();
+            if (value.Length > 20)
+                throw new InvalidHouseNumberLengthException(20);
+
+            Value = value;  
+        }
+    }
+}
