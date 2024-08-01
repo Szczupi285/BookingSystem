ea667a21909c74f3057a8060222e181164795fee
unknown <bartekbiexd@gmail.com>
2024-08-01 18:49:49 +0200

    feat: EmployeeId value object

---
+++ src/BookingSystem.Domain/Abstractions/BookingDomainException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingSystem.Domain.Abstractions
+{
+    public abstract class BookingDomainException : Exception
+    {
+        protected BookingDomainException(string message) : base(message) { }
+    }
+}
+
---
+++ src/BookingSystem.Domain/ValueObjects/Employee/EmployeeId.cs
@@ -0,0 +1,25 @@
+﻿using BookingSystem.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingSystem.Domain.ValueObjects.Employee
+{
+    public record EmployeeId
+    {
+        public Guid Value { get; }
+        public EmployeeId(Guid value)
+        {
+            if(value == Guid.Empty)
+                throw new EmptyEmployeeIdException();
+
+            Value = value;
+        }
+
+        public static implicit operator Guid(EmployeeId Id) => Id.Value;
+
+        public static implicit operator EmployeeId(Guid Id) => new(Id);
+    }
+}
