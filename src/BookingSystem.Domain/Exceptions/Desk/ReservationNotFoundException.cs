2efec8c64980adb5acfb6f62e00b1933c0313066
unknown <bartekbiexd@gmail.com>
2024-08-02 14:57:56 +0200

    fix: fixed typo in ReservationNotFoundException

--- src/BookingSystem.Domain/Exceptions/Desk/ReservationNotFoundException.cs
+++ src/BookingSystem.Domain/Exceptions/Desk/ReservationNotFoundException.cs
@@ -10,5 +10,8 @@ namespace BookingSystem.Domain.Exceptions.Desk
     public class ReservationNotFoundException : BookingDomainException
     {
         public ReservationNotFoundException(Guid guid) : base($"Reservation with Id: {guid} has not been found")
+        {
+        }
+
     }
 }
---
+++ src/BookingSystem.Domain/Repositories/ILocationRepository.cs
@@ -0,0 +1,20 @@
+﻿using BookingSystem.Domain.Entities;
+using BookingSystem.Domain.ValueObjects.Location;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingSystem.Domain.Repositories
+{
+    public interface ILocationRepository
+    {
+        Task<Location> GetByIdAsync(LocationId Id);
+        Task<IEnumerable<Location>> GetAllAsync();
+        Task AddAsync(Location location);
+        Task UpdateAsync(Location location);
+        Task RemoveAsync(Location location);
+        Task SaveChangesAsync(CancellationToken cancellation);
+    }
+}
